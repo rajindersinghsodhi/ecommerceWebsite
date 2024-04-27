a3d61a2559434257b4b0cda9efe05ada31f0d519
Rajinder Singh Sodhi <rajsodhisocial@gmail.com>
2024-04-27 12:01:39 +0530

    header routing completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, createComponent } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SellerComponent } from './components/seller/seller.component';
+import { CartComponent } from './components/cart/cart.component';
+import { LoginComponent } from './components/login/login.component';
+import { SingupComponent } from './components/singup/singup.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "seller", component: SellerComponent},
+  {path: "cart", component: CartComponent},
+  {path: "login", component: LoginComponent},
+  {path: "signup", component: SingupComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SellerComponent } from './components/seller/seller.component';
+import { CartComponent } from './components/cart/cart.component';
+import { LoginComponent } from './components/login/login.component';
+import { SingupComponent } from './components/singup/singup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SellerComponent,
+    CartComponent,
+    LoginComponent,
+    SingupComponent
   ],
   imports: [
     BrowserModule,
